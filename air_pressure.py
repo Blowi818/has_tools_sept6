e75d7c4640b708f9db13eda64d487854a2b3af08
Blowi <mohammedalbalawi@arizona.edu>
2022-09-06 09:11:02 -0700

    class script from today (sept 6)

---
+++ air_pressure.py
@@ -0,0 +1,20 @@
+import math
+
+
+def air_pressure_at_height(h):
+    p0 = 101325       # reference pressure in pascals
+    M = 0.02896968    # molar mass of air kg/nol
+    g = 9.81          # gravity m/s2
+    R0 = 8.314462618  # gas constant J(mol*K)
+    T = 272           # temp in kelven
+
+    ratio = -(g*h*M)/(R0*T)
+    p_h = p0 * math.exp(ratio)
+    return p_h
+
+
+# list of elevation to run
+h_list = range(0, 1000, 100)
+for height in h_list:
+    p_h = air_pressure_at_height(height)
+    print(height, '    ', p_h)
